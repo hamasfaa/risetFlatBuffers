f275b7788cb090f6220c7d4b0925d6b688b0c76c
hamasfaa <hamasfa@gmail.com>
2024-11-21 06:21:46 +0700

    tambah 2-way

--- src/robot/src/comm.cpp
+++ src/robot/src/comm.cpp
@@ -3,6 +3,8 @@
 #include <flatbuffers/flatbuffers.h>
 #include <fstream>
 #include <netinet/in.h>
+#include <stdio.h>
+#
 #include <sys/socket.h>
 #include <unistd.h>
 #include "robot/PC2BS.h"
@@ -29,7 +31,7 @@ void sendComm(const ros::TimerEvent &)
     tsb.add_x(xTurtle);
     tsb.add_y(yTurtle);
     tsb.add_theta(thetaTurtle);
-    printf("xTurtle: %f, yTurtle: %f, thetaTurtle: %f\n", xTurtle, yTurtle, thetaTurtle);
+    printf("[C++] Sending: x: %f, y: %f, theta: %f\n", xTurtle, yTurtle, thetaTurtle);
     auto ts = tsb.Finish();
 
     builder.Finish(ts);
@@ -40,6 +42,32 @@ void sendComm(const ros::TimerEvent &)
     send(new_socket, buf, size, 0);
 }
 
+void recvComm(const ros::TimerEvent &)
+{
+    uint8_t buffer[1024];
+    int bytesReceived = recv(new_socket, buffer, sizeof(buffer), MSG_DONTWAIT);
+
+    if (bytesReceived > 0)
+    {
+        flatbuffers::Verifier verifier(buffer, bytesReceived);
+
+        if (verifier.VerifyBuffer<TurtleSim::TurtleStatus>(nullptr))
+        {
+            auto ts = flatbuffers::GetRoot<TurtleSim::TurtleStatus>(buffer);
+
+            float x = ts->x();
+            float y = ts->y();
+            float theta = ts->theta();
+
+            printf("[C++] Received: x: %f, y: %f, theta: %f\n", x, y, theta);
+        }
+        else
+        {
+            printf("[C++] Invalid buffer.\n");
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "comm");
@@ -48,29 +76,34 @@ int main(int argc, char **argv)
     ros::Subscriber subPC2BS = nh.subscribe<robot::PC2BS>("pc2bs_topic", 1, pc2bsCallback);
 
     int server_fd;
-    struct sockaddr_in address;
+    struct sockaddr_in addr;
     int opt = 1;
-    int addrlen = sizeof(address);
+    int addrlen = sizeof(addr);
 
     // =============================
-    // SERVER
+    // SERVER & CLIENT
     // =============================
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-
-    // =============================
-    // CLIENT
-    // =============================
-    // otw
-
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+    bind(server_fd, (struct sockaddr *)&addr, sizeof(addr));
     listen(server_fd, 3);
 
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    printf("[C++] Waiting for client connection...\n");
+    new_socket = accept(server_fd, (struct sockaddr *)&addr, (socklen_t *)&addrlen);
+    printf("[C++] Client connected.\n");
 
     ros::Timer timer = nh.createTimer(ros::Duration(0.02), sendComm);
+
+    // ros::Rate rate(50);
+    // while (ros::ok())
+    // {
+    //     ros::spinOnce();
+    //     recvComm();
+    //     rate.sleep();
+    // }
+    ros::Timer timer2 = nh.createTimer(ros::Duration(0.02), recvComm);
 
     ros::spin();
 

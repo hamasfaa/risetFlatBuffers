0b9e2a473d57e03b185c5addeb16824c8cbcb84a
hamasfaa <hamasfa@gmail.com>
2024-11-20 08:21:23 +0700

    fix data kosong

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const flatbuffers = require('flatbuffers');
 const turtleSim = require('./turtle_sim').TurtleSim;
 
 const client = new net.Socket();
-client.connect(9999, '127.0.0.1', () => {
+client.connect(1234, '127.0.0.1', () => {
     console.log('Connected');
 });
 
 client.on('data', (data) => {
-    const buf = new flatbuffers.ByteBuffer(new Uint8Array(data));
-    const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
+    if (data.length > 0) {
+        const buf = new flatbuffers.ByteBuffer(new Uint8Array(data));
+        const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
 
-    console.log('Received: ', turtleStatus.x(), turtleStatus.y(), turtleStatus.theta());
+        console.log('Received: ', turtleStatus.x(), turtleStatus.y(), turtleStatus.theta());
+    }
 });
 
 client.on('close', () => {

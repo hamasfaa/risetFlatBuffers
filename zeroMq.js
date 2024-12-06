a090cff60c227d2e2ef32a24495f7e2487239aa9
hamasfaa <hamasfa@gmail.com>
2024-12-07 05:22:47 +0700

    pub-sub tcp

--- zeroMq.js
+++ zeroMq.js
@@ -3,20 +3,34 @@ const flatbuffers = require('flatbuffers');
 const turtleSim = require('./turtle_sim').TurtleSim;
 
 async function run() {
-    const sock = new zmq.Pull();
+    const sock = new zmq.Subscriber();
 
     sock.connect("tcp://localhost:5555");
-    console.log("JavaScript: Waiting for message...");
 
-    for await (const [msg] of sock) {
-        const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
-        const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
+    sock.subscribe("status");
 
-        let x = turtleStatus.x();
-        let y = turtleStatus.y();
-        let theta = turtleStatus.theta();
+    console.log("JavaScript: Waiting for messages on 'status'...");
 
-        console.log(`[JS] Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
+    for await (const [topic, msg] of sock) {
+        if (topic.toString() == 'status') {
+            const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
+            const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
+
+            let x = turtleStatus.x();
+            let y = turtleStatus.y();
+            let theta = turtleStatus.theta();
+
+            console.log(`[JS] 1 Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
+        } else if (topic.toString() == 'status2') {
+            const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
+            const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
+
+            let x = turtleStatus.x();
+            let y = turtleStatus.y();
+            let theta = turtleStatus.theta();
+
+            console.log(`[JS] 2 Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
+        }
     }
 }
 

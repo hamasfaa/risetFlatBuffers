80b5cf53c739e08d44d8f03ec1f3482458a320ea
hamasfaa <hamasfa@gmail.com>
2024-12-06 07:32:04 +0700

    push-pull

--- zeroMq.js
+++ zeroMq.js
@@ -1,4 +1,6 @@
 const zmq = require('zeromq');
+const flatbuffers = require('flatbuffers');
+const turtleSim = require('./turtle_sim').TurtleSim;
 
 async function run() {
     const sock = new zmq.Pull();
@@ -7,7 +9,14 @@ async function run() {
     console.log("JavaScript: Waiting for message...");
 
     for await (const [msg] of sock) {
-        console.log("JavaScript: Received message:", msg.toString());
+        const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
+        const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
+
+        let x = turtleStatus.x();
+        let y = turtleStatus.y();
+        let theta = turtleStatus.theta();
+
+        console.log(`[JS] Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
     }
 }
 

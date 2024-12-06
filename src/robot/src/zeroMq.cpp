5ddafb8d188149e08115ad7b5f5058419a336718
hamasfaa <hamasfa@gmail.com>
2024-12-07 05:22:21 +0700

    pub-sub tcp

--- src/robot/src/zeroMq.cpp
+++ src/robot/src/zeroMq.cpp
@@ -6,31 +6,58 @@
 int main()
 {
     zmq::context_t context(1);
-    zmq::socket_t socket(context, zmq::socket_type::push);
-    socket.bind("tcp://*:5555");
+    zmq::socket_t publisher(context, ZMQ_PUB);
+    publisher.bind("tcp://*:5555");
 
-    float xTurtle = 1.0;
-    float yTurtle = 2.0;
-    float thetaTurtle = 3.0;
+    while (true)
+    {
+        float xTurtle = 1.0;
+        float yTurtle = 2.0;
+        float thetaTurtle = 3.0;
 
-    flatbuffers::FlatBufferBuilder builder;
+        flatbuffers::FlatBufferBuilder builder;
 
-    TurtleSim::TurtleStatusBuilder tsb(builder);
-    tsb.add_x(xTurtle);
-    tsb.add_y(yTurtle);
-    tsb.add_theta(thetaTurtle);
+        TurtleSim::TurtleStatusBuilder tsb(builder);
+        tsb.add_x(xTurtle);
+        tsb.add_y(yTurtle);
+        tsb.add_theta(thetaTurtle);
 
-    auto ts = tsb.Finish();
+        auto ts = tsb.Finish();
+        builder.Finish(ts);
 
-    builder.Finish(ts);
+        uint8_t *buf = builder.GetBufferPointer();
+        int size = builder.GetSize();
 
-    uint8_t *buf = builder.GetBufferPointer();
-    int size = builder.GetSize();
+        zmq::message_t topic_msg("status", 6);
+        zmq::message_t msg(buf, size);
 
-    zmq::message_t message(buf, size);
+        publisher.send(topic_msg, zmq::send_flags::sndmore);
+        publisher.send(msg, zmq::send_flags::none);
 
-    socket.send(message, zmq::send_flags::none);
+        float xTurtle2 = 4.0;
+        float yTurtle2 = 5.0;
+        float thetaTurtle2 = 6.0;
 
-    std::cout << "C++: Sent message: Hello, world" << std::endl;
+        flatbuffers::FlatBufferBuilder builder2;
+
+        TurtleSim::TurtleStatusBuilder tsb2(builder2);
+        tsb2.add_x(xTurtle2);
+        tsb2.add_y(yTurtle2);
+        tsb2.add_theta(thetaTurtle2);
+
+        auto ts2 = tsb2.Finish();
+        builder2.Finish(ts2);
+
+        uint8_t *buf2 = builder2.GetBufferPointer();
+        int size2 = builder2.GetSize();
+
+        zmq::message_t topic_msg2("status2", 7);
+        zmq::message_t msg2(buf2, size2);
+
+        publisher.send(topic_msg2, zmq::send_flags::sndmore);
+        publisher.send(msg2, zmq::send_flags::none);
+
+        std::cout << "C++: Sent message on topic 'status'" << std::endl;
+    }
     return 0;
 }

6f917d5dbd73a678b152cec749188ba25e3c9846
hamasfaa <hamasfa@gmail.com>
2024-12-06 07:32:17 +0700

    push pull

--- src/robot/src/zeroMq.cpp
+++ src/robot/src/zeroMq.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <zmq.hpp>
+#include <flatbuffers/flatbuffers.h>
+#include "robot/turtle_sim_generated.h"
 
 int main()
 {
@@ -7,7 +9,26 @@ int main()
     zmq::socket_t socket(context, zmq::socket_type::push);
     socket.bind("tcp://*:5555");
 
-    zmq::message_t message("Hello, world", 12);
+    float xTurtle = 1.0;
+    float yTurtle = 2.0;
+    float thetaTurtle = 3.0;
+
+    flatbuffers::FlatBufferBuilder builder;
+
+    TurtleSim::TurtleStatusBuilder tsb(builder);
+    tsb.add_x(xTurtle);
+    tsb.add_y(yTurtle);
+    tsb.add_theta(thetaTurtle);
+
+    auto ts = tsb.Finish();
+
+    builder.Finish(ts);
+
+    uint8_t *buf = builder.GetBufferPointer();
+    int size = builder.GetSize();
+
+    zmq::message_t message(buf, size);
+
     socket.send(message, zmq::send_flags::none);
 
     std::cout << "C++: Sent message: Hello, world" << std::endl;

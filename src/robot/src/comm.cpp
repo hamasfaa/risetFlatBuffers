97e66d3ccd60a12e238c8e56a2508d1bdba732aa
hamasfaa <hamasfa@gmail.com>
2024-12-11 20:49:49 +0700

    try js to cpp

--- src/robot/src/comm.cpp
+++ src/robot/src/comm.cpp
@@ -1,32 +1,19 @@
 #include <ros/ros.h>
 #include <robot/turtle_sim_generated.h>
+#include <zmq.hpp>
 #include <flatbuffers/flatbuffers.h>
-#include <fstream>
-#include <netinet/in.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <unistd.h>
 #include "robot/PC2BS.h"
 #include "robot/BS2PC.h"
 
-#define PORT 1234
-
 float xTurtle, yTurtle, thetaTurtle;
 
-int udp_socket;
-
-// =============================
-// CLIENT
-// =============================
-struct sockaddr_in client_addr;
-socklen_t client_len = sizeof(client_addr);
-
-// =============================
-// SERVER
-// =============================
-struct sockaddr_in server_addr;
-
 ros::Publisher pubBS2PC;
+
+zmq::context_t context(1);
+zmq::socket_t publisher(context, ZMQ_PUB);
+zmq::socket_t subscriber(context, ZMQ_SUB);
 
 void pc2bsCallback(const robot::PC2BS::ConstPtr &msg)
 {
@@ -44,7 +31,6 @@ void sendComm(const ros::TimerEvent &)
     tsb.add_x(xTurtle);
     tsb.add_y(yTurtle);
     tsb.add_theta(thetaTurtle);
-    // printf("[C++] Sending: x: %f, y: %f, theta: %f\n", xTurtle, yTurtle, thetaTurtle);
     auto ts = tsb.Finish();
 
     builder.Finish(ts);
@@ -52,36 +38,40 @@ void sendComm(const ros::TimerEvent &)
     uint8_t *buf = builder.GetBufferPointer();
     int size = builder.GetSize();
 
-    sendto(udp_socket, buf, size, 0, (struct sockaddr *)&client_addr, client_len);
+    zmq::message_t topic_msg("status", 6);
+    zmq::message_t msg(buf, size);
+
+    publisher.send(topic_msg, zmq::send_flags::sndmore);
+    publisher.send(msg, zmq::send_flags::none);
 }
 
 void recvComm(const ros::TimerEvent &)
 {
-    uint8_t buffer[1024];
-    int bytesReceived = recvfrom(udp_socket, buffer, sizeof(buffer), MSG_DONTWAIT, (struct sockaddr *)&client_addr, &client_len);
+    zmq::message_t sub_msg;
+    zmq::message_t sub_topic;
 
-    if (bytesReceived > 0)
+    subscriber.recv(sub_topic, zmq::recv_flags::none);
+    subscriber.recv(sub_msg, zmq::recv_flags::none);
+
+    std::string topicStr(static_cast<char *>(topic.data()), topic.size());
+
+    if (topicStr == "control")
     {
+        uint8_t *buffer = static_cast<uint8_t *>(sub_msg.data());
+        int bytesReceived = sub_msg.size();
+
         flatbuffers::Verifier verifier(buffer, bytesReceived);
+        auto cc = flatbuffers::GetRoot<TurtleSim::ControlCommand>(buffer);
 
-        if (verifier.VerifyBuffer<TurtleSim::ControlCommand>(nullptr))
-        {
-            auto cc = flatbuffers::GetRoot<TurtleSim::ControlCommand>(buffer);
+        float linear_velocity = cc->linear_velocity();
+        float angular_velocity = cc->angular_velocity();
 
-            float linear_velocity = cc->linear_velocity();
-            float angular_velocity = cc->angular_velocity();
+        printf("[C++] Received: linear_velocity: %f, angular_velocity: %f\n", linear_velocity, angular_velocity);
 
-            printf("[C++] Received: linear_velocity: %f, angular_velocity: %f\n", linear_velocity, angular_velocity);
-
-            robot::BS2PC msg;
-            msg.linear_velocity = linear_velocity;
-            msg.angular_velocity = angular_velocity;
-            pubBS2PC.publish(msg);
-        }
-        else
-        {
-            printf("[C++] Invalid buffer.\n");
-        }
+        robot::BS2PC msg;
+        msg.linear_velocity = linear_velocity;
+        msg.angular_velocity = angular_velocity;
+        pubBS2PC.publish(msg);
     }
 }
 
@@ -90,23 +80,17 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "comm");
     ros::NodeHandle nh;
 
+    publisher.bind("tcp://*:5555");
+    subscriber.connect("tcp://localhost:5555");
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, "control", 7);
+
     pubBS2PC = nh.advertise<robot::BS2PC>("bs2pc_topic", 1);
     ros::Subscriber subPC2BS = nh.subscribe<robot::PC2BS>("pc2bs_topic", 1, pc2bsCallback);
 
-    udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    bind(udp_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    ros::Timer timer = nh.createTimer(ros::Duration(0.02), sendComm);
-    ros::Timer timer2 = nh.createTimer(ros::Duration(0.02), recvComm);
+    ros::Timer timer2 = nh.createTimer(ros::Duration(0.02), sendComm);
+    // ros::Timer timer2 = nh.createTimer(ros::Duration(0.02), recvComm);
 
     ros::spin();
-
-    close(udp_socket);
 
     return 0;
 }
--- src/robot/src/zeroMq.cpp
+++ src/robot/src/zeroMq.cpp
@@ -3,61 +3,87 @@
 #include <flatbuffers/flatbuffers.h>
 #include "robot/turtle_sim_generated.h"
 
+// int main()
+// {
+//     zmq::context_t context(1);
+//     zmq::socket_t publisher(context, ZMQ_PUB);
+//     publisher.bind("tcp://*:5555");
+
+//     while (true)
+//     {
+//         float xTurtle = 1.0;
+//         float yTurtle = 2.0;
+//         float thetaTurtle = 3.0;
+
+//         flatbuffers::FlatBufferBuilder builder;
+
+//         TurtleSim::TurtleStatusBuilder tsb(builder);
+//         tsb.add_x(xTurtle);
+//         tsb.add_y(yTurtle);
+//         tsb.add_theta(thetaTurtle);
+
+//         auto ts = tsb.Finish();
+//         builder.Finish(ts);
+
+//         uint8_t *buf = builder.GetBufferPointer();
+//         int size = builder.GetSize();
+
+//         zmq::message_t topic_msg("status", 6);
+//         zmq::message_t msg(buf, size);
+
+//         publisher.send(topic_msg, zmq::send_flags::sndmore);
+//         publisher.send(msg, zmq::send_flags::none);
+
+//         float xTurtle2 = 4.0;
+//         float yTurtle2 = 5.0;
+//         float thetaTurtle2 = 6.0;
+
+//         flatbuffers::FlatBufferBuilder builder2;
+
+//         TurtleSim::TurtleStatusBuilder tsb2(builder2);
+//         tsb2.add_x(xTurtle2);
+//         tsb2.add_y(yTurtle2);
+//         tsb2.add_theta(thetaTurtle2);
+
+//         auto ts2 = tsb2.Finish();
+//         builder2.Finish(ts2);
+
+//         uint8_t *buf2 = builder2.GetBufferPointer();
+//         int size2 = builder2.GetSize();
+
+//         zmq::message_t topic_msg2("status2", 7);
+//         zmq::message_t msg2(buf2, size2);
+
+//         publisher.send(topic_msg2, zmq::send_flags::sndmore);
+//         publisher.send(msg2, zmq::send_flags::none);
+
+//         std::cout << "C++: Sent message on topic 'status'" << std::endl;
+//     }
+//     return 0;
+// }
+
 int main()
 {
     zmq::context_t context(1);
-    zmq::socket_t publisher(context, ZMQ_PUB);
-    publisher.bind("tcp://*:5555");
+    zmq::socket_t subscriber(context, ZMQ_SUB);
+    subscriber.connect("tcp://localhost:5556");
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, "kitty cats", 10);
 
     while (true)
     {
-        float xTurtle = 1.0;
-        float yTurtle = 2.0;
-        float thetaTurtle = 3.0;
+        zmq::message_t sub_msg;
+        zmq::message_t sub_topic;
 
-        flatbuffers::FlatBufferBuilder builder;
+        subscriber.recv(sub_topic, zmq::recv_flags::none);
+        subscriber.recv(sub_msg, zmq::recv_flags::none);
 
-        TurtleSim::TurtleStatusBuilder tsb(builder);
-        tsb.add_x(xTurtle);
-        tsb.add_y(yTurtle);
-        tsb.add_theta(thetaTurtle);
+        std::string topicStr(static_cast<char *>(sub_topic.data()), sub_topic.size());
+        std::string messageStr(static_cast<char *>(sub_msg.data()), sub_msg.size());
 
-        auto ts = tsb.Finish();
-        builder.Finish(ts);
-
-        uint8_t *buf = builder.GetBufferPointer();
-        int size = builder.GetSize();
-
-        zmq::message_t topic_msg("status", 6);
-        zmq::message_t msg(buf, size);
-
-        publisher.send(topic_msg, zmq::send_flags::sndmore);
-        publisher.send(msg, zmq::send_flags::none);
-
-        float xTurtle2 = 4.0;
-        float yTurtle2 = 5.0;
-        float thetaTurtle2 = 6.0;
-
-        flatbuffers::FlatBufferBuilder builder2;
-
-        TurtleSim::TurtleStatusBuilder tsb2(builder2);
-        tsb2.add_x(xTurtle2);
-        tsb2.add_y(yTurtle2);
-        tsb2.add_theta(thetaTurtle2);
-
-        auto ts2 = tsb2.Finish();
-        builder2.Finish(ts2);
-
-        uint8_t *buf2 = builder2.GetBufferPointer();
-        int size2 = builder2.GetSize();
-
-        zmq::message_t topic_msg2("status2", 7);
-        zmq::message_t msg2(buf2, size2);
-
-        publisher.send(topic_msg2, zmq::send_flags::sndmore);
-        publisher.send(msg2, zmq::send_flags::none);
-
-        std::cout << "C++: Sent message on topic 'status'" << std::endl;
+        if (topicStr == "kitty cats")
+        {
+            std::cout << "Received: " << messageStr << std::endl;
+        }
     }
     return 0;
 }

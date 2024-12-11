04a6f765259c61a76afb4124bab5f03eff09ddbf
hamasfaa <hamasfa@gmail.com>
2024-12-11 20:49:35 +0700

    try js to cpp

--- script.js
+++ script.js
@@ -1,69 +1,30 @@
-const dgram = require('dgram');
+const zmq = require('zeromq');
 const flatbuffers = require('flatbuffers');
 const turtleSim = require('./turtle_sim').TurtleSim;
-const readline = require('readline');
 
-const PORT = 1234;
-const HOST = '127.0.0.1';
-const client = dgram.createSocket('udp4');
+async function run() {
+    const subSock = new zmq.Subscriber();
+    const pubSock = new zmq.Publisher();
 
-function createTurtleStatus(linear, angular) {
-    const builder = new flatbuffers.Builder(1024);
+    subSock.connect("tcp://localhost:5555");
+    pubSock.bind("tcp://*:5556");
 
-    turtleSim.ControlCommand.startControlCommand(builder);
-    turtleSim.ControlCommand.addLinearVelocity(builder, linear);
-    turtleSim.ControlCommand.addAngularVelocity(builder, angular);
+    subSock.subscribe("status");
 
-    const controlCommand = turtleSim.ControlCommand.endControlCommand(builder);
+    console.log("JavaScript: Waiting for messages on 'status'...");
 
-    builder.finish(controlCommand);
+    for await (const [topic, msg] of subSock) {
+        if (topic.toString() == 'status') {
+            const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
+            const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
 
-    return builder.asUint8Array();
+            let x = turtleStatus.x();
+            let y = turtleStatus.y();
+            let theta = turtleStatus.theta();
+
+            console.log(`[JS] Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
+        }
+    }
 }
 
-client.on('message', (msg, rinfo) => {
-    const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
-    const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
-
-    let x = turtleStatus.x();
-    let y = turtleStatus.y();
-    let theta = turtleStatus.theta();
-
-    console.log(`[JS] Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
-});
-
-client.on('error', (err) => {
-    console.error(`[JS] Error: ${err.message}`);
-    client.close();
-});
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.on('line', (input) => {
-    let linearVelocity = 0;
-    let angularVelocity = 0;
-
-    switch (input) {
-        case 'w':
-            linearVelocity = 1;
-            break;
-        case 's':
-            linearVelocity = -1;
-            break;
-        case 'a':
-            angularVelocity = 1;
-            break;
-        case 'd':
-            angularVelocity = -1;
-            break;
-        default:
-            console.log('Invalid input. Use w/s for linear and a/d for angular velocity.');
-            return;
-    }
-
-    const response = createTurtleStatus(linearVelocity, angularVelocity);
-    client.send(response, 0, response.length, PORT, HOST)
-});
+run();
--- zeroMq.js
+++ zeroMq.js
@@ -2,35 +2,42 @@ const zmq = require('zeromq');
 const flatbuffers = require('flatbuffers');
 const turtleSim = require('./turtle_sim').TurtleSim;
 
+// async function run() {
+//     const sock = new zmq.Subscriber();
+
+//     sock.connect("tcp://localhost:5555");
+
+//     sock.subscribe("status");
+
+//     console.log("JavaScript: Waiting for messages on 'status'...");
+
+//     for await (const [topic, msg] of sock) {
+//         if (topic.toString() == 'status') {
+//             const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
+//             const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
+
+//             let x = turtleStatus.x();
+//             let y = turtleStatus.y();
+//             let theta = turtleStatus.theta();
+
+//             console.log(`[JS] 1 Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
+//         }
+//     }
+// }
+
+// run();
+
 async function run() {
-    const sock = new zmq.Subscriber();
+    const sock = new zmq.Publisher();
 
-    sock.connect("tcp://localhost:5555");
+    await sock.bind("tcp://*:5556");
 
-    sock.subscribe("status");
-
-    console.log("JavaScript: Waiting for messages on 'status'...");
-
-    for await (const [topic, msg] of sock) {
-        if (topic.toString() == 'status') {
-            const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
-            const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
-
-            let x = turtleStatus.x();
-            let y = turtleStatus.y();
-            let theta = turtleStatus.theta();
-
-            console.log(`[JS] 1 Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
-        } else if (topic.toString() == 'status2') {
-            const buf = new flatbuffers.ByteBuffer(new Uint8Array(msg));
-            const turtleStatus = turtleSim.TurtleStatus.getRootAsTurtleStatus(buf);
-
-            let x = turtleStatus.x();
-            let y = turtleStatus.y();
-            let theta = turtleStatus.theta();
-
-            console.log(`[JS] 2 Received from Server: x: ${x}, y: ${y}, theta: ${theta}`);
-        }
+    while (true) {
+        console.log("sending a multipart message envelope")
+        await sock.send(["kitty cats", "meow!"])
+        await new Promise(resolve => {
+            setTimeout(resolve, 500)
+        })
     }
 }
 
